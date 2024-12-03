41b024d82a13b4675e38971e4acb4d57eab81d5b
Pavel Hess <pavelhess@gmail.com>
2024-12-03 19:08:17 +0100

    first commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"/", "/home", "/status"})
     public String home() {
-        return "Hello World!!!!!";
+        return "Hello World!!!!! Tak ahoj světe";
     }
 }
